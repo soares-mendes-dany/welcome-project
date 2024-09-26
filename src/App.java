472c44c49187365041bcf88c88eda061d5b75a84
soares-mendes-dany <dany.soares.001@student.uni.lu>
2024-09-26 07:59:48 +0200

    update ex1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to BINFO!");
+        System.out.println("Welcome to BINFO!!");
     }
 }
